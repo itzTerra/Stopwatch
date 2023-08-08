a65273411d6ae9c8cd61b98f3179ddf8704ff88f
Michal Doležel <michaldolezel123@gmail.com>
2023-08-08 23:39:13 +0200

    Apache ECharts integration

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
-const removeButton = '<button type="button" class="btn-close" aria-label="Close"></button>';
+const removeButton =
+  '<button type="button" class="btn-close" aria-label="Close"></button>';
 
 const minLabel = document.getElementById("min");
 const minutesLabel = document.getElementById("minutes");
@@ -6,15 +7,44 @@ const secLabel = document.getElementById("sec");
 const msLabel = document.getElementById("ms");
 
 const toggleButton = document.getElementById("startbtn");
-const saveButton = document.getElementById("savebtn")
+const saveButton = document.getElementById("savebtn");
 const resetButton = document.getElementById("resetbtn");
 
 const avgTimeLabel = document.getElementById("avg-time");
 const timeCountLabel = document.getElementById("time-count");
 
-const $historyTable = $('#history-table');
+const $historyTable = $("#history-table");
 const historyTableBody = document.querySelector("#history-table tbody");
 
+const timesChart = echarts.init(document.getElementById("timesChart"), "dark");
+timesChart.setOption({
+  title: {
+    text: "Progression Chart",
+  },
+  tooltip: {
+    trigger: "axis",
+  },
+  legend: {
+    data: ["legend"],
+  },
+  xAxis: {
+    type: "time",
+    name: "Date",
+  },
+  yAxis: {
+    type: "time",
+    name: "Time",
+  },
+  dataset: {
+    dimensions: ["date", "time"],
+    source: [],
+  },
+  //   series: [
+  //     {
+  //       type: "line",
+  //     },
+  //   ],
+});
 
 var timeID = 0;
 
@@ -29,147 +59,172 @@ var timerInterval;
 var started = false;
 var shownMinutes = false;
 
-var timeHistory = {}
+var timeHistory = {};
 
 window.removeEvent = {
-    "click .btn-close": (e, value, row, index) => {
-        var removeID = row.id;
-        delete timeHistory[removeID];
-        localStorage.setItem("timeHistory", JSON.stringify(timeHistory));
+  "click .btn-close": (e, value, row, index) => {
+    var removeID = row.id;
+    delete timeHistory[removeID];
+    localStorage.setItem("timeHistory", JSON.stringify(timeHistory));
 
-        $historyTable.bootstrapTable('removeByUniqueId', removeID)
-        updateAvg(-row.timeMS);
-        updateTimeCount();
-    }
-}
+    $historyTable.bootstrapTable("removeByUniqueId", removeID);
+    updateAvg(-row.timeMS);
+    updateTimeCount();
+  },
+};
 
 $historyTable.bootstrapTable();
 
 // Get history of measured times from localStorage
 var timeHistorySaved = JSON.parse(localStorage.getItem("timeHistory"));
-if (timeHistorySaved !== null){
-    for (var rec of Object.values(timeHistorySaved)){
-        appendHistoryRow(rec.date, rec.time, rec.ms);
-    }
+if (timeHistorySaved !== null) {
+  for (var rec of Object.values(timeHistorySaved)) {
+    appendHistoryRow(rec.date, rec.time, rec.ms);
+  }
 }
 
 function startStop() {
-    if (!started){
-        // Start timer
-        startTime = Date.now() - elapsedTime;
-        timerInterval = setInterval(updateTimer, 10);
-        toggleButton.textContent = "Stop";
-        saveButton.disabled = true;
-    } else{
-        // Stop timer
-        clearInterval(timerInterval);
-        toggleButton.textContent = "Start";
-        saveButton.disabled = false;
-    }
-    started = !started;  
+  if (!started) {
+    // Start timer
+    startTime = Date.now() - elapsedTime;
+    timerInterval = setInterval(updateTimer, 10);
+    toggleButton.textContent = "Stop";
+    saveButton.disabled = true;
+  } else {
+    // Stop timer
+    clearInterval(timerInterval);
+    toggleButton.textContent = "Start";
+    saveButton.disabled = false;
+  }
+  started = !started;
 }
 
 function reset() {
-    clearInterval(timerInterval);
-    elapsedTime = 0;
-    started = false;
-    
-    minutesLabel.style.display = "none";
-    shownMinutes = false;
+  clearInterval(timerInterval);
+  elapsedTime = 0;
+  started = false;
 
-    minLabel.textContent = "";
-    secLabel.textContent = 0;
-    msLabel.textContent = "00";
+  minutesLabel.style.display = "none";
+  shownMinutes = false;
 
-    toggleButton.textContent = "Start";
-    saveButton.disabled = true;
+  minLabel.textContent = "";
+  secLabel.textContent = 0;
+  msLabel.textContent = "00";
+
+  toggleButton.textContent = "Start";
+  saveButton.disabled = true;
 }
 
 function updateTimer() {
-    var currentTime = Date.now();
-    elapsedTime = currentTime - startTime;
-    var time = new Date(elapsedTime);
-    var minutes = time.getUTCMinutes().toString();
-    var seconds = time.getUTCSeconds().toString();
-    var milliseconds = Math.floor(time.getUTCMilliseconds() / 10).toString().padStart(2, "0");
+  var currentTime = Date.now();
+  elapsedTime = currentTime - startTime;
+  var time = new Date(elapsedTime);
+  var minutes = time.getUTCMinutes().toString();
+  var seconds = time.getUTCSeconds().toString();
+  var milliseconds = Math.floor(time.getUTCMilliseconds() / 10)
+    .toString()
+    .padStart(2, "0");
 
-    if (!shownMinutes && minutes > 0){
-        minutesLabel.style.display = "inline";
-        shownMinutes = true;
-    }
+  if (!shownMinutes && minutes > 0) {
+    minutesLabel.style.display = "inline";
+    shownMinutes = true;
+  }
 
-    minLabel.textContent = minutes > 0 ? minutes : "";
-    secLabel.textContent = minutes > 0 ? seconds.padStart(2, "0") : seconds;
-    msLabel.textContent = milliseconds;
+  minLabel.textContent = minutes > 0 ? minutes : "";
+  secLabel.textContent = minutes > 0 ? seconds.padStart(2, "0") : seconds;
+  msLabel.textContent = milliseconds;
 }
 
-function save(){
-    var curDate = new Date().toLocaleDateString("en-gb")
-    var time = new Date(elapsedTime);
-    var minutes = time.getUTCMinutes().toString().padStart(2, "0");
-    var seconds = time.getUTCSeconds().toString().padStart(2, "0");
-    var milliseconds = Math.floor(time.getUTCMilliseconds() / 10).toString().padStart(2, "0");
-    time = `${minutes}:${seconds}:${milliseconds}`;
+function save() {
+  var curDate = new Date().toLocaleDateString("en-gb");
+  var time = new Date(elapsedTime);
+  var minutes = time.getUTCMinutes().toString().padStart(2, "0");
+  var seconds = time.getUTCSeconds().toString().padStart(2, "0");
+  var milliseconds = Math.floor(time.getUTCMilliseconds() / 10)
+    .toString()
+    .padStart(2, "0");
+  time = `${minutes}:${seconds}:${milliseconds}`;
 
-    appendHistoryRow(curDate, time, elapsedTime);
-    saveTimeHistory();
+  appendHistoryRow(curDate, time, elapsedTime);
+  saveTimeHistory();
 }
 
-function saveTimeHistory(){
-    localStorage.setItem("timeHistory", JSON.stringify(timeHistory));
+function saveTimeHistory() {
+  localStorage.setItem("timeHistory", JSON.stringify(timeHistory));
 }
 
-function appendHistoryRow(date, time, timeMs){
-    timeHistory[timeID] = {
-        "date": date, 
-        "time": time, 
-        "ms": timeMs
-    };
+function appendHistoryRow(date, time, timeMs) {
+  timeHistory[timeID] = {
+    date: date,
+    time: time,
+    ms: timeMs,
+  };
 
-    var removeID = timeID++;
+  var removeID = timeID++;
 
-    $historyTable.bootstrapTable('insertRow', {index: 0, row: {
-        id: removeID,
-        date: date,
-        time: time,
-        timeMS: timeMs,
-        remove: ""
-    }});
+  $historyTable.bootstrapTable("insertRow", {
+    index: 0,
+    row: {
+      id: removeID,
+      date: date,
+      time: time,
+      timeMS: timeMs,
+      remove: "",
+    },
+  });
 
-    updateAvg(timeMs);
-    updateTimeCount();
+  const dateParts = date.split("/");
+  const year = parseInt(dateParts[2]);
+  const month = parseInt(dateParts[1]) - 1; // Months in JavaScript are 0-indexed
+  const day = parseInt(dateParts[0]);
+
+  const dateObject = new Date(year, month, day);
+
+  timesChart.setOption({
+    dataset: {
+      source: [dateObject, timeMs],
+    },
+  });
+
+  updateAvg(timeMs);
+  updateTimeCount();
 }
 
-function updateAvg(value){
-    timeSum += value;
-    if (value > 0){
-        timeCount++
-    } else if (value < 0){
-        timeCount--
-    }
+function updateAvg(value) {
+  timeSum += value;
+  if (value > 0) {
+    timeCount++;
+  } else if (value < 0) {
+    timeCount--;
+  }
 
-    if (timeCount == 0){
-        avgTimeLabel.textContent = "0";
-        return
-    }
-    let average = timeSum / timeCount;
+  if (timeCount == 0) {
+    avgTimeLabel.textContent = "0";
+    return;
+  }
+  let average = timeSum / timeCount;
 
-    let minutes = Math.floor(average/60/1000).toString().padStart(2, "0");
-    let seconds = Math.floor((average % (60 * 1000)) / 1000).toString().padStart(2, "0");
-    let milliseconds = Math.floor((average % 1000) / 10).toString().padStart(2, "0");
+  let minutes = Math.floor(average / 60 / 1000)
+    .toString()
+    .padStart(2, "0");
+  let seconds = Math.floor((average % (60 * 1000)) / 1000)
+    .toString()
+    .padStart(2, "0");
+  let milliseconds = Math.floor((average % 1000) / 10)
+    .toString()
+    .padStart(2, "0");
 
-    avgTimeLabel.textContent = minutes + ":" + seconds + ":" + milliseconds;
+  avgTimeLabel.textContent = minutes + ":" + seconds + ":" + milliseconds;
 }
 
-function updateTimeCount(){
-    timeCountLabel.textContent = timeCount;
+function updateTimeCount() {
+  timeCountLabel.textContent = timeCount;
 }
 
-function removeFormatter(value, row, index){
-    return removeButton;
+function removeFormatter(value, row, index) {
+  return removeButton;
 }
 
 // window.onbeforeunload += () => {
 //     localStorage.setItem("timeHistory", JSON.stringify(timeHistory));
 // }
-
